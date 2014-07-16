beb5e3d54ebffb352e5da387f6f8e74794cf5828
mobeets <mobeets@gmail.com>
2014-07-16 16:59:31 -0500

    sorting; added src url

--- intriguing-things.js
+++ intriguing-things.js
@@ -1,18 +1,19 @@
 function dt2str(val) {
   var date = new Date(val*1000);
   var d1 = date.getDate();
-  var d2 = date.getMonth() + 1; //Months are zero based
+  var d2 = date.getMonth() + 1;
   var d3 = date.getFullYear();
   return d3 + "-" + d2 + "-" + d1;
 }
-function li_item(dt, vals) {
+function li_item(key, dt, vals) {
     var ts = [];
     for (i in vals){
         out0 = "<div class=\"item-title\"><span class=\"dt\">" + dt2str(dt) + "</span>";
-        out1 = "<span class=\"num\">(" + vals[i].number + ")</span></div>";
-        out2 = "<div class=\"title\"><a href=\"" + vals[i].url + "\">" + vals[i].title + "</a></div>";
-        out3 = "<div class=\"ps\">" + vals[i].ps.join("") + "</div>";
-        ts.push("<div class=\"item\">" + out0 + out1 + out2 + out3 + "</div>");
+        out1 = "<span class=\"num\">#" + vals[i].number + "</span>";
+        out2 = "<span class=\"src-url\"><a href=\"" + vals[i].src_url + "\">[source]</a></span></div>";
+        out3 = "<div class=\"title\"><a href=\"" + vals[i].url + "\">" + vals[i].title + "</a></div>";
+        out4 = "<div class=\"ps\">" + vals[i].ps.join("") + "</div>";
+        ts.push("<div id=\"" + dt + "-" + vals[i].number + "\" class=\"item\">" + out0 + out1 + out2 + out3 + out4 + "</div>");
     }
     return ts.join("<hr>");
 }
@@ -20,7 +21,7 @@ function li_item(dt, vals) {
 function add_to_list(data) {
   var out = [];
   $.each(data, function(key, val) {
-    out.push(li_item(val[0], val[1]));
+    out.push(li_item(key, val[0], val[1]));
   });
  
   $("<div/>", {
@@ -30,8 +31,6 @@ function add_to_list(data) {
 }
 
 var url = "data.json";
-// var url = "http://raw.githubusercontent.com/mobeets/intriguing-things/master/intriguing-things.json";
 $(function() {
-    console.log('Hello!');
     $.getJSON(url, add_to_list);
 });
--- model.py
+++ model.py
@@ -1,5 +1,8 @@
+import glob
 import json
+import os.path
 import urllib2
+import argparse
 import datetime
 from time import mktime
 from dateutil import parser
@@ -7,7 +10,20 @@ from BeautifulSoup import BeautifulSoup
 
 BASE_URL = 'http://tinyletter.com/intriguingthings/letters/'
 EXAMPLE_URL =  'http://tinyletter.com/intriguingthings/letters/5-intriguing-things-146'
-START_URL = 'http://tinyletter.com/intriguingthings/letters/5-intriguing-things-like-a-dog-in-an-mri-machine'
+RESTART_URL = 'http://tinyletter.com/intriguingthings/letters/5-intriguing-things-like-a-dog-in-an-mri-machine'
+NEXT_URL = 'http://tinyletter.com/intriguingthings/letters/5-intriguing-things-like-a-dog-in-an-mri-machine'
+
+class Thing:
+    def __init__(self, number, title, url):
+        self.number = number
+        self.title = title
+        self.url = url
+        self.ps = []
+        self.src_url = None
+
+    def __str__(self):
+        ps = u'\n<br>'.join([p.decode('utf-8') for p in self.ps])
+        return u'<a href="{0}">{1}</a>: {2}'.format(self.url, self.title, ps)
 
 def read(url):
     response = urllib2.urlopen(url)
@@ -21,32 +37,23 @@ def parse(html):
     next_url = next_url.get('href') if next_url is not None else next_url
     return dt, contents, next_url
 
-class Thing:
-    def __init__(self, number, title, url):
-        self.number = number
-        self.title = title
-        self.url = url
-        self.ps = []
-
-    def __str__(self):
-        ps = u'\n<br>'.join([p.decode('utf-8') for p in self.ps])
-        return u'<a href="{0}">{1}</a>: {2}'.format(self.url, self.title, ps)
-
-def things(obj):
-    breaks = ["""1957 American English""", """Today's 1957""", """Tell Your Friends: Subscribe to 5 Intri""", """Did some good soul forward you this email?""", """Were you forwarded this email?""" """1957 English Usage""" """Subscribe to 5 Intriguing Things"""]
+def things(obj, src_url):
+    breaks = ["""1957 American English""", """Today's 1957""", """Tell Your Friends: Subscribe to 5 Intri""", """Did some good soul forward you this email?""", """Were you forwarded this email?""", """1957 English Usage""", """Subscribe to 5 Intriguing Things"""]
     i = 1
     thing = None
     items = []
     found_number = lambda i, val: val.startswith('{0}.'.format(i))
     has_number = lambda i, p: (p.strong is not None and found_number(i, p.strong.text)) or found_number(i, p.text)
+    has_url = lambda p: p.a is not None and p.a.has_key('href')
     for p in obj.findChildren('p'):
         if p.text is None:
             continue
-        if any([has_number(j, p) for j in xrange(i, i+2)]): # for various numbering errors
+        if any([has_number(j, p) for j in xrange(i, i+2)]) and has_url(p): # for various numbering errors
             if thing is not None:
                 items.append(thing)
                 thing = None
-            thing = Thing(i, p.a.text if p.a is not None else p.text.partition('. ')[-1], p.a.get('href') if (p.a is not None and p.a.has_key('href')) else '#')
+            thing = Thing(i, p.a.text if p.a is not None else p.text.partition('. ')[-1], p.a.get('href'))
+            thing.src_url = src_url
             i += 1
         elif thing is not None:
             if [brk for brk in breaks if brk in p.text]:
@@ -68,23 +75,31 @@ def load(url=EXAMPLE_URL):
     dt, contents, next_url = parse(read(url))
     if dt.strftime('%Y-%m-%d') == '2014-06-06':
         contents = fix_2014_06_06(read(url))
-    return (dt, things(contents)), next_url
+    return (dt, things(contents, url)), next_url
 
-class MyEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, datetime.datetime):
-            return int(mktime(obj.timetuple()))
-        elif isinstance(obj, Thing):
-            return obj.__dict__
-        return json.JSONEncoder.default(self, obj)
+def write(T, outfile, merge):
+    class MyEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, datetime.datetime):
+                return int(mktime(obj.timetuple()))
+            elif isinstance(obj, Thing):
+                return obj.__dict__
+            return json.JSONEncoder.default(self, obj)
 
-def write(T, outfile):
-    with open(outfile, 'w') as f:
-        json.dump(T, f, cls=MyEncoder)
+    out = json.dumps(T, cls=MyEncoder)
+    if merge and os.path.exists(outfile):
+        with open(outfile) as f:
+            out_p = json.load(f)
+        out_c = []
+        for x,y in json.loads(out) + out_p:
+            if x not in out_c:
+                out_c.append((x,y))
+        out = out_c
+    open(outfile, 'w').write(out)
 
-def main(starturl=START_URL, outfile='tmp.json'):
+def main(starturl, outfile, merge):
     """
-    * also want all hrefs in the entire html contents probably
+    * want to store source url 
     """
     T = []
     next_url = starturl
@@ -95,26 +110,33 @@ def main(starturl=START_URL, outfile='tmp.json'):
         if len(ts) == 0 and dt.strftime('%Y-%m-%d') not in ['2014-06-06']:
             print 'ERROR: {0}'.format(dt)
         T.append(ts)
-    write(T, outfile)
+    write(T, outfile, merge)
 
-def tmp_main(outfile='tmp.json'):
-    import glob
-    import os.path
+def tmp_main(starturl, outfile, merge):
     T = []
     for infile in glob.glob('int_things_raw/*'):
         html = open(infile).read()
-        dt, contents, next_url = parse(html)
-        ts = things(contents)
+        dt, contents, _ = parse(html)
+        src_url = infile.replace('___', '/').replace('int_things_raw/', '')
+        ts = things(contents, src_url)
         if len(ts) == 0:
             if dt.strftime('%Y-%m-%d') == '2013-12-17':
-                ts = things(fix_2014_06_06(html))
+                ts = things(fix_2014_06_06(html), src_url)
             elif dt.strftime('%Y-%m-%d') == '2014-06-06':
-                pass # 1/0 # pass
+                pass
             else:
                 print 'ERROR: {0}'.format(dt)
         T.append((dt, ts))
-    write(T, outfile)
+    write(T, outfile, merge)
 
 if __name__ == '__main__':
-    # tmp_main()
-    main()
+    psr = argparse.ArgumentParser()
+    psr.add_argument('--outfile', default='tmp.json')
+    psr.add_argument('--restart', action='store_true', default=False)
+    psr.add_argument('--local', action='store_true', default=False)
+    psr.add_argument('--merge', action='store_true', default=False)
+    args = psr.parse_args()
+    if args.local:
+        tmp_main(RESTART_URL if args.restart else NEXT_URL, args.outfile, args.merge)
+    else:
+        main(RESTART_URL if args.restart else NEXT_URL, args.outfile, args.merge)
