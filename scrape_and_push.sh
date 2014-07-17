18b4d38f852b3ed8b7513c33fed2713731970f07
mobeets <mobeets@gmail.com>
2014-07-17 18:38:16 -0500

    reverting data to test

---
+++ scrape_and_push.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+repo="https://github.com/mobeets/intriguing-things.git"
+now=$(date +"%m_%d_%Y")
+echo $repo
+echo $now
+
+# git clone "$repository" .
+# python model.py --infile data.json --outfile.json
+# git add .
+# git commit -m "data update ($now)"
+# git push origin gh-pages
