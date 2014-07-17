f9b5839cc5f9832365fbb3fa40005b762e26feca
mobeets <mobeets@gmail.com>
2014-07-16 22:14:55 -0500

    buttons! yay

--- intriguing-things.js
+++ intriguing-things.js
@@ -1,3 +1,5 @@
+var keys = [];
+
 function dt2str(val) {
   var date = new Date(val*1000);
   var d1 = date.getDate();
@@ -13,12 +15,17 @@ function li_item(key, dt, vals) {
         out2 = "<span class=\"src-url\"><a href=\"" + vals[i].src_url + "\">[source]</a></span></div>";
         out3 = "<div class=\"title\"><a href=\"" + vals[i].url + "\">" + vals[i].title + "</a></div>";
         out4 = "<div class=\"ps\">" + vals[i].ps.join("") + "</div>";
-        ts.push("<div id=\"" + dt + "-" + vals[i].number + "\" class=\"item\">" + out0 + out1 + out2 + out3 + out4 + "</div>");
+
+        cur_key = dt + "-" + vals[i].number;
+        ts.push("<div id=\"" + cur_key + "\" class=\"item\">" + out0 + out1 + out2 + out3 + out4 + "</div>");
+        keys.push(cur_key);
     }
-    return ts.join("<hr>");
+    return ts.join("\n");
 }
 
 function add_to_list(data) {
+  data.sort(function(a,b) { return parseFloat(b[0]) - parseFloat(a[0]) } );
+
   var out = [];
   $.each(data, function(key, val) {
     out.push(li_item(key, val[0], val[1]));
@@ -26,11 +33,32 @@ function add_to_list(data) {
  
   $("<div/>", {
     "id": "items",
-    html: out.join("<hr>")
+    html: out.join("\n")
   }).appendTo(".container");
+}
+
+function show_random() {
+  $("#items").hide("fast");
+  $("#rand-items").show("fast");
+  inds = [];
+  i = 1;
+  while (i < 6) {
+    ind = Math.floor(Math.random() * keys.length);
+    if ($.inArray(ind, inds)) {
+      inds.push(ind);
+      $("#rand-item-" + i).html($('#' + keys[ind]).html());
+      i += 1;
+    }
+  }
+}
+function show_all() {
+  $("#items").show("fast");
+  $("#rand-items").hide("fast");
 }
 
 var url = "data.json";
 $(function() {
     $.getJSON(url, add_to_list);
+    $("#show-random").click(show_random);
+    $("#show-all").click(show_all);
 });
