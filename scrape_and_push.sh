338154f78611eed78cdfba7df6559f2b05fe286f
mobeets <mobeets@gmail.com>
2014-07-21 23:09:50 -0500

    procfile

--- scrape_and_push.sh
+++ scrape_and_push.sh
@@ -2,10 +2,12 @@
 
 now=$(date +"%Y/%m/%d")
 git clone https://github.com/mobeets/intriguing-things.git
+# git clone ssh://git@github.com/mobeets/intriguing-things.git
 cd intriguing-things
 python model.py --infile data.json --outfile data.json
 git add .
 git commit -m "data update ($now)"
+git remote set-url origin https://$GITHUB_USERNAME:$GITHUB_PASSWORD@github.org/intriguing-things.git
 git push origin gh-pages
 cd ..
 rm -rf intriguing-things
