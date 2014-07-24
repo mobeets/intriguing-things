7459537a0799815af71a28f428336ffcae73264e
mobeets <mobeets@gmail.com>
2014-07-23 20:56:47 -0500

    okay, back to start for a bit

---
+++ intriguing-things-new.js
@@ -0,0 +1,62 @@
+var keys = [];
+
+function li_item(val) {
+  out0 = "<div class=\"item-title\"><span class=\"dt\">" + val.dt + "</span>";
+  out1 = "<span class=\"num\">#" + val.number + "</span>";
+  out2 = "<span class=\"src-url\"><a href=\"" + val.src_url + "\">[source]</a></span></div>";
+  out3 = "<div class=\"title\"><a href=\"" + val.url + "\">" + val.title + "</a></div>";
+  out4 = "<div class=\"ps\">" + val.ps + "</div>";
+
+  cur_key = val.index;
+  keys.push(cur_key);
+  return "<div id=\"" + cur_key + "\" class=\"item\">" + out0 + out1 + out2 + out3 + out4 + "</div>";
+}
+
+function str2dt(val) {
+  if (val.indexOf(".") == -1) { return new Date(1900, 1, 1); }
+  if (val.indexOf("-") == -1) { return new Date(1900, 1, 1); }
+  end = val.split(".");
+  vals = end[0].split("-");
+  return new Date(vals[0], vals[1], vals[2], parseFloat(end[1]));
+}
+
+function add_to_list(data) {
+  data.sort(function(a,b) { return str2dt(b.dt) - str2dt(a.dt); } );
+  data = data.reverse();
+
+  var out = [];
+  $.each(data, function(key, val) {
+    out.push(li_item(val));
+  });
+ 
+  $("<div/>", {
+    "id": "items",
+    html: out.join("\n")
+  }).appendTo(".container");
+}
+
+function show_random() {
+  $("#items").hide("fast");
+  $("#rand-items").show("fast");
+  inds = [];
+  i = 1;
+  while (i < 6) {
+    ind = Math.floor(Math.random() * keys.length);
+    if ($.inArray(ind, inds)) {
+      inds.push(ind);
+      $("#rand-item-" + i).html($('#' + keys[ind]).html());
+      i += 1;
+    }
+  }
+}
+function show_all() {
+  $("#items").show("fast");
+  $("#rand-items").hide("fast");
+}
+
+var url = "data.json";
+$(function() {
+    $.getJSON(url, add_to_list);
+    $("#show-random").click(show_random);
+    $("#show-all").click(show_all);
+});
--- intriguing-things.js
+++ intriguing-things.js
@@ -1,32 +1,34 @@
 var keys = [];
 
-function li_item(val) {
-  out0 = "<div class=\"item-title\"><span class=\"dt\">" + val.dt + "</span>";
-  out1 = "<span class=\"num\">#" + val.number + "</span>";
-  out2 = "<span class=\"src-url\"><a href=\"" + val.src_url + "\">[source]</a></span></div>";
-  out3 = "<div class=\"title\"><a href=\"" + val.url + "\">" + val.title + "</a></div>";
-  out4 = "<div class=\"ps\">" + val.ps + "</div>";
-
-  cur_key = val.index;
-  keys.push(cur_key);
-  return "<div id=\"" + cur_key + "\" class=\"item\">" + out0 + out1 + out2 + out3 + out4 + "</div>";
+function dt2str(val) {
+  var date = new Date(val*1000);
+  var d1 = date.getDate();
+  var d2 = date.getMonth() + 1;
+  var d3 = date.getFullYear();
+  return d3 + "-" + d2 + "-" + d1;
 }
+function li_item(key, dt, vals) {
+    var ts = [];
+    for (i in vals){
+        out0 = "<div class=\"item-title\"><span class=\"dt\">" + dt2str(dt) + "</span>";
+        out1 = "<span class=\"num\">#" + vals[i].number + "</span>";
+        out2 = "<span class=\"src-url\"><a href=\"" + vals[i].src_url + "\">[source]</a></span></div>";
+        out3 = "<div class=\"title\"><a href=\"" + vals[i].url + "\">" + vals[i].title + "</a></div>";
+        out4 = "<div class=\"ps\">" + vals[i].ps.join("") + "</div>";
 
-function str2dt(val) {
-  if (val.indexOf(".") == -1) { return new Date(1900, 1, 1); }
-  if (val.indexOf("-") == -1) { return new Date(1900, 1, 1); }
-  end = val.split(".");
-  vals = end[0].split("-");
-  return new Date(vals[0], vals[1], vals[2], parseFloat(end[1]));
+        cur_key = dt + "-" + vals[i].number;
+        ts.push("<div id=\"" + cur_key + "\" class=\"item\">" + out0 + out1 + out2 + out3 + out4 + "</div>");
+        keys.push(cur_key);
+    }
+    return ts.join("\n");
 }
 
 function add_to_list(data) {
-  data.sort(function(a,b) { return str2dt(b.dt) - str2dt(a.dt); } );
-  data = data.reverse();
+  data.sort(function(a,b) { return parseFloat(b[0]) - parseFloat(a[0]) } );
 
   var out = [];
   $.each(data, function(key, val) {
-    out.push(li_item(val));
+    out.push(li_item(key, val[0], val[1]));
   });
  
   $("<div/>", {
